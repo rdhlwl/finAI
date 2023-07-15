3b43a602564cb3127d35941c45e947303f2d8234
Ravnoor Dhaliwal <107385633+rdhlwl@users.noreply.github.com>
2023-07-14 19:02:34 -0700

    ai functional

--- fin.py
+++ fin.py
@@ -1,11 +1,42 @@
-import streamlit
+from dotenv import load_dotenv
+import tempfile
+import csv
+import streamlit as st
+from langchain.agents import create_csv_agent
+from langchain.llms import OpenAI
 
 
 def main():
-    streamlit.set_page_config(page_title="Fin.AI", page_icon="🤖")
-    streamlit.header("Fin.AI: Your AI-Powered Personal Finance Coach 💸")
 
-    inputcsv = streamlit.file_uploader("Upload your bank statement CSV", type = "csv")
+    load_dotenv()
+
+    st.set_page_config(page_title="Fin.AI", page_icon="🤖")
+    st.header("Fin.AI: Your AI-Powered Personal Finance Coach 💸")
+
+    inputcsv = st.file_uploader("Upload your bank statement CSV", type = "csv")
+
+    if inputcsv is not None:
+
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".csv") as f:
+            f.write(inputcsv.getvalue())
+
+            llm = OpenAI(temperature=0)
+            agent = create_csv_agent(llm, f.name, verbose = True)
+
+            qchoice1 = st.button("Show me a chart of my expenses")
+            qchoice2 = st.button("Breakdown my spending by category")
+            qchoice3 = st.button("Breakdown and give feedback on my spending")
+        
+            if qchoice1:
+                st.write("📈1")
+            if qchoice2:
+                response = agent.run("Parse through the transactions in the CSV file and sum up all of the transactions and sort them into their respective spending categories and sort by highest amount spent to lowest.")
+                st.write(response)
+
+# def process_csv(inputfile):
+#     with open(inputfile, 'r') as input
+
+        
 
 
 
